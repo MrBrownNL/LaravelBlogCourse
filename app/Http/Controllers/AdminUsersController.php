cd9173b8eb1a12f5b3099c4f27a95eaf990545c7
Jeroen de Bruijn <jeroen@alphega.nl>
2019-12-11 20:04:41 +0100

    Finished edit user form

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\EditUsersRequest;
 use App\Http\Requests\UsersRequest;
 use App\Photo;
 use App\Role;
@@ -108,9 +109,31 @@ class AdminUsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(EditUsersRequest $request, $id)
     {
         //
+        $user = User::findOrFail($id);
+        $input = $request->all();
+        if ($file = $request->file('photo_id')) {
+            $name = time() . $file->getClientOriginalName();
+
+            $file->move('images', $name);
+
+            $photo = Photo::create(['file'=>$name]);
+
+            $input['photo_id'] = $photo->id;
+             }
+
+        if ($input['password'] == '')
+                $input['password'] = $user->password;
+            else
+                $input['password'] = bcrypt($request->password);
+
+        $user->update($input);
+
+        return redirect('/admin/users');
+
+
     }
 
     /**
--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -5,44 +5,55 @@
 
     <h1>Edit user</h1>
 
-    {!! Form::model($user,['method'=>'PATCH','action'=>['AdminUsersController@update', $user->id],'files'=>true]) !!}
-
-    <div class="form-group">
-        {!! Form::label('name','Naam:') !!}
-        {!! Form::text('name', null, [ 'class'=>'form-control']) !!}
+    <div class="col-sm-3">
+        @if($user->photo)
+            <img height="50" src="{{ $user->photo->file}}">
+        @else
+            No picture set<br>
+        @endif
     </div>
+    <br>
+    <div class="col-sm-9">
 
-    <div class="form-group">
-        {!! Form::label('email','E-mail:') !!}
-        {!! Form::text('email', null, [ 'class'=>'form-control']) !!}
+        {!! Form::model($user,['method'=>'PATCH','action'=>['AdminUsersController@update', $user->id],'files'=>true]) !!}
+
+        <div class="form-group">
+            {!! Form::label('name','Naam:') !!}
+            {!! Form::text('name', null, [ 'class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('email','E-mail:') !!}
+            {!! Form::text('email', null, [ 'class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('photo_id','Foto:') !!}
+            {!! Form::file('photo_id', null, [ 'class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('role_id','Rol:') !!}
+            {!! Form::select('role_id', $roles, null, ['class'=>'form-control select2minimal']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('is_active','Status:') !!}
+            {!! Form::select('is_active',[1=>'Actief',0=>'Niet actief'], null, [ 'class'=>'form-control select2minimal']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('password','Wachtwoord:') !!}
+            {!! Form::password('password', [ 'class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::submit('Opslaan',['class'=>'btn btn-primary']) !!}
+        </div>
+
+        {!! Form::close() !!}
+
     </div>
-
-    <div class="form-group">
-        {!! Form::label('photo_id','Foto:') !!}
-        {!! Form::file('photo_id', null, [ 'class'=>'form-control']) !!}
-    </div>
-
-    <div class="form-group">
-        {!! Form::label('role_id','Rol:') !!}
-        {!! Form::select('role_id', $roles, null, ['class'=>'form-control select2minimal']) !!}
-    </div>
-
-    <div class="form-group">
-        {!! Form::label('is_active','Status:') !!}
-        {!! Form::select('is_active',[1=>'Actief',0=>'Niet actief'], null, [ 'class'=>'form-control select2minimal']) !!}
-    </div>
-
-    <div class="form-group">
-        {!! Form::label('password','Wachtwoord:') !!}
-        {!! Form::password('password', [ 'class'=>'form-control']) !!}
-    </div>
-
-    <div class="form-group">
-        {!! Form::submit('Gebruiker aanmaken',['class'=>'btn btn-primary']) !!}
-    </div>
-
-    {!! Form::close() !!}
-
 
     @include('includes.form_errors')
 
